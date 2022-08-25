68951bdf5d24696d55cfd6d94bb1fa3dd282b7c6
chenghua.wang <2923277184@qq.com>
2022-08-25 21:27:55 +0800

    Add script to download CUDA11.7

---
+++ script/install_CUDA11_7_WSL2.sh
@@ -0,0 +1,9 @@
+wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-wsl-ubuntu.pin
+sudo mv cuda-wsl-ubuntu.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/11.7.0/local_installers/cuda-repo-wsl-ubuntu-11-7-local_11.7.0-1_amd64.deb
+sudo dpkg -i cuda-repo-wsl-ubuntu-11-7-local_11.7.0-1_amd64.deb
+sudo cp /var/cuda-repo-wsl-ubuntu-11-7-local/cuda-*-keyring.gpg /usr/share/keyrings/
+sudo apt-get update
+sudo apt-get -y install cuda
+sudo echo "export PATH=/usr/local/cuda-11.7/bin:$PATH >> ~/.bashrc"
+sudo echo "export LD_LIBRARY_PATH=/usr/local/cuda-11.7/lib64:$LD_LIBRARY_PATH >> ~/.bashrc"
